cd073e316dc26fe6b916c4ac851f8e305f1903ee
Kyle Pace <kpace2296@gmail.com>
2020-03-19 20:11:49 -0500

    testing

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 // const Data = require('./data/db') 
 const postsRouter = require('./postsRouter');
-const cors = require('cors');
+// const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
-server.use(cors());
+// server.use(cors());
 server.use('/api/posts', postsRouter)
 
 server.get('/', (req, res) => {
